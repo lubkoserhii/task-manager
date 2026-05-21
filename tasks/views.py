4a9e1aec757b449e83f995a0d0192a25d93747da
lubkoserhii <lubko.serhii@gmail.com.com>
2026-05-21 18:33:45 +0300

    Add Views

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,108 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    TemplateView,
+    UpdateView,
+)
+
+from .models import Position, Task, TaskType, Worker
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = "tasks/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["num_tasks"] = Task.objects.count()
+        context["num_completed_tasks"] = Task.objects.filter(
+            is_completed=True
+        ).count()
+        context["num_workers"] = Worker.objects.count()
+        return context
+
+
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    paginate_by = 5
+    queryset = Task.objects.select_related("task_type").prefetch_related(
+        "assignees"
+    )
+
+
+class TaskDetailView(LoginRequiredMixin, DetailView):
+    model = Task
+    queryset = Task.objects.select_related("task_type").prefetch_related(
+        "assignees"
+    )
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = (
+        "name",
+        "description",
+        "deadline",
+        "is_completed",
+        "priority",
+        "task_type",
+        "assignees",
+    )
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    fields = (
+        "name",
+        "description",
+        "deadline",
+        "is_completed",
+        "priority",
+        "task_type",
+        "assignees",
+    )
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    model = Task
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class WorkerListView(LoginRequiredMixin, ListView):
+    model = Worker
+    paginate_by = 10
+    queryset = Worker.objects.select_related("position")
+
+
+class WorkerDetailView(LoginRequiredMixin, DetailView):
+    model = Worker
+    queryset = Worker.objects.select_related("position")
+
+
+class PositionListView(LoginRequiredMixin, ListView):
+    model = Position
+    paginate_by = 10
+
+
+class PositionCreateView(LoginRequiredMixin, CreateView):
+    model = Position
+    fields = ("name",)
+    success_url = reverse_lazy("tasks:position-list")
+
+
+class TaskTypeListView(LoginRequiredMixin, ListView):
+    model = TaskType
+    paginate_by = 10
+    template_name = "tasks/task_type_list.html"
+
+
+class TaskTypeCreateView(LoginRequiredMixin, CreateView):
+    model = TaskType
+    fields = ("name",)
+    success_url = reverse_lazy("tasks:task-type-list")
+    template_name = "tasks/task_type_form.html"
