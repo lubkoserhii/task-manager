9fa714b038b4db9fa8fed5cf13511788460cbc55
lubkoserhii <lubko.serhii@gmail.com.com>
2026-05-22 17:39:49 +0300

    Refactor views

--- tasks/views.py
+++ tasks/views.py
@@ -11,31 +11,7 @@ from django.views.generic import (
 )
 
 from .models import Position, Task, TaskType, Worker
-
-
-class SearchMixin:
-    search_param = "q"
-    search_fields = ()
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        search_query = self.request.GET.get(self.search_param, "").strip()
-
-        if not search_query:
-            return queryset
-
-        filters = Q()
-        for field in self.search_fields:
-            filters |= Q(**{f"{field}__icontains": search_query})
-
-        return queryset.filter(filters).distinct()
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["search_query"] = self.request.GET.get(
-            self.search_param, ""
-        ).strip()
-        return context
+from .searchMixin import SearchMixin
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
